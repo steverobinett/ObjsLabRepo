8ce8ff997eec6fa83a1c2ba49fa4e4a686c8f37f
Steve Robinett <steven.robinett@gfcmsu.edu>
2022-11-20 15:01:39 -0700

    init

---
+++ javaScriptObjectsLab/objects.js
@@ -0,0 +1,38 @@
+// Joke Object
+var joke = {
+  categories: [],
+  created_at: "2020-01-05 13:42:27.496799",
+  icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+  id: "Lpj9bMdWSvCMPvZWGhN7Dg",
+  updated_at: "2020-01-05 13:42:27.496799",
+  url: "https://api.chucknorris.io/jokes/Lpj9bMdWSvCMPvZWGhN7Dg",
+  value:
+    "Chuck Norris graduated from preschool, high school, and college before he was even born.",
+};
+
+
+var movie = {
+  title: "Die Hard",
+  cast: ["Bruce Willis", "Alan Rickman", "Bonnie Bedelia"],
+  reviewer: {
+    revName: "Steve",
+    stars: 4,
+  },
+};
+// --- Using movie object
+
+// ----
+var courses = {
+  CSCI: {
+    100: "Intro to Programming",
+    105: "Computer Fluency",
+    111: "Java Programming",
+  },
+  ITS: {
+    210: "Network OS Desktop",
+    218: "Network Security",
+    224: "Intro to Linux",
+  },
+};
+
+// Using the Courses Object
